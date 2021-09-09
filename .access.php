dd7e331dbdd0f248681f00eb2881add77d5df48a
Alexey Chichkanov <kolob_ok@bk.ru>
2021-09-09 16:52:40 +0300

    диллер

--- .access.php
+++ .access.php
@@ -1,4 +1,14 @@
 <?
-$PERM["obozrevatel-obektov"]["5"]="R";
+$PERM["obozrevatel-obektov"]["3"]="R";
+
+$PERM["batteries"]["5"]="R";
+$PERM["clients"]["5"]="R";
+$PERM["calendar"]["5"]="R";
+$PERM["documents"]["5"]="R";
+
+
+$PERM["/"]["3"]="R";
 $PERM["/"]["*"]="D";
+
+
 ?>
