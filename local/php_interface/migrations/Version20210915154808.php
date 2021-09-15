1cac49f0b5fea9e43dc81cbb5817714c01e93a4d
/usr/bin/vim <dev@vedita.ru>
2021-09-15 16:08:45 +0300

    миграции с данными аккамуляторов

---
+++ local/php_interface/migrations/Version20210915154808.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Sprint\Migration;
+
+
+class Version20210915154808 extends Version
+{
+    protected $description = "hlblock элементы 15.09";
+
+    protected $moduleVersion = "3.28.7";
+
+    /**
+     * @throws Exceptions\ExchangeException
+     * @throws Exceptions\RestartException
+     * @throws Exceptions\HelperException
+     * @return bool|void
+     */
+    public function up()
+    {
+        $this->getExchangeManager()
+            ->HlblockElementsImport()
+            ->setExchangeResource('hlblock_elements.xml')
+            ->setLimit(20)
+            ->execute(function ($item) {
+                $this->getHelperManager()
+                    ->Hlblock()
+                    ->addElement(
+                        $item['hlblock_id'],
+                        $item['fields']
+                    );
+            });
+    }
+
+    public function down()
+    {
+        //your code ...
+    }
+}
