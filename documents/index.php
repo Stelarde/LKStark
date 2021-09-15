fe0352893a8faab8e02e9ee3889f9990aa0577a0
Alexandr Ovchinin <fromdz@mail.ru>
2021-09-15 16:17:20 +0300

    commit

--- documents/index.php
+++ documents/index.php
@@ -3,7 +3,7 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Документация");
 ?><main class="main">
 <div class="main-internal">
-	 <?$APPLICATION->IncludeComponent(
+	<?$APPLICATION->IncludeComponent(
 	"bitrix:menu",
 	"top",
 	Array(
