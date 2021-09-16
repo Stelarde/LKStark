6bdc0e5cfcd90fd8b345a992a1d77d6fd006e947
Alexey Chichkanov <kolob_ok@bk.ru>
2021-09-16 14:43:16 +0300

    добавил favicon

--- clients/index.php
+++ clients/index.php
@@ -1,6 +1,6 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetTitle("Title");
+$APPLICATION->SetTitle("Клиенты");
 ?><main class="main">
 <div class="main-internal">
 	 <?$APPLICATION->IncludeComponent(
